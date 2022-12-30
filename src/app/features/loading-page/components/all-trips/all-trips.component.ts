887dfe6bdce1e1c9e61b9b6e510f78257be67cc1
islam-farahat <islam.mostafa.f@gmail.com>
2022-12-30 10:42:11 +0200

    updates

--- src/app/features/loading-page/components/all-trips/all-trips.component.ts
+++ src/app/features/loading-page/components/all-trips/all-trips.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import * as moment from 'moment';
 import { ITrip } from '../../models/trip.model';
 import { TravelRegisterService } from '../../services/travel-register.service';
 
@@ -9,11 +11,31 @@ import { TravelRegisterService } from '../../services/travel-register.service';
 })
 export class AllTripsComponent implements OnInit {
   trips: ITrip[] = [];
-  constructor(private travel: TravelRegisterService) {}
+  constructor(private travel: TravelRegisterService, private fb: FormBuilder) {}
 
   ngOnInit(): void {
     this.travel.getTrips().subscribe((buses) => {
       this.trips = buses;
     });
+  }
+  day(day: string) {
+    return moment(day).format('dddd');
+  }
+
+  billDocument = this.fb.group({
+    from: ['', Validators.required],
+    to: ['', Validators.required],
+  });
+
+  filterBillDocument() {
+    this.trips.splice(0);
+    this.travel
+      .findTripByDate(
+        moment(this.billDocument.value.from!).format('YYYY-MM-DD'),
+        moment(this.billDocument.value.to!).format('YYYY-MM-DD')
+      )
+      .subscribe((obj) => {
+        this.trips = obj;
+      });
   }
 }
