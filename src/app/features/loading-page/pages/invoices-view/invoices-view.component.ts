6a1d3d12115cc75152881f1df28c8d5e16298a86
mustafa ashraf <mustafaashraf744@gmail.com>
2023-02-17 22:27:49 +0300

    ..

--- src/app/features/loading-page/pages/invoices-view/invoices-view.component.ts
+++ src/app/features/loading-page/pages/invoices-view/invoices-view.component.ts
@@ -234,7 +234,7 @@ export class InvoicesViewComponent implements OnInit {
       align: 'center',
     });
     pdf.text(['AL-KASSAR',
-    ], 30, 10, {
+    ], 50, 10, {
       align: 'center',
     });
     pdf.setFontSize(8);
@@ -251,7 +251,7 @@ export class InvoicesViewComponent implements OnInit {
       'Tel : 0177253157 Office : 0559738321',
       'Mob : 0504760563 - 0552836914',
       'Al-Baha-Main ST. opp. Extra'
-    ], 45, 15, {
+    ], 40, 15, {
       align: 'center',
     });
 
